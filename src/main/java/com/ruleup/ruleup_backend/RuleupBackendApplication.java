092bab9d25b9919c568a0bcd7204e5c0ceda51b8
selee1012 <dltjddms1012@gmail.com>
2026-05-26 16:46:56 +0900

    chore: initial commit from Spring Initializr

---
+++ src/main/java/com/ruleup/ruleup_backend/RuleupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ruleup.ruleup_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleupBackendApplication.class, args);
+	}
+
+}
